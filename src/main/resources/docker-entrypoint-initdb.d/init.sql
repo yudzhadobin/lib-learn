37cb8d0aeac2120b2ac5d031e5766deaf0039d9c
yuriy <yudzhadobin@gmail.com>
2017-10-08 11:03:53 +0300

    Initial commit

---
+++ src/main/resources/docker-entrypoint-initdb.d/init.sql
@@ -0,0 +1,10 @@
+CREATE USER Yura;
+
+CREATE TABLE COMPANY(
+   ID INT PRIMARY KEY     NOT NULL,
+   NAME           TEXT    NOT NULL,
+   AGE            INT     NOT NULL,
+   ADDRESS        CHAR(50),
+   SALARY         REAL
+);
+
